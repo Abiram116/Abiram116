fad3f6ed55031f3736888d1c0621106ecb8c7246
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2023-11-22 19:23:27 +0530

    error

--- in_sem-2/Structures/struct_arr.c
+++ in_sem-2/Structures/struct_arr.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
-struct student{
+
+struct student {
     int id;
     char name[100];
     float fees;
-    char branch[5];
+    char branch[50]; // Increased size to accommodate longer branch names
 };
-int main(){
+
+int main() {
     int n;
     printf("Enter the number of elements in the array: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
+
     struct student arr[n];
-    for(int i=0;i<n;i++){
+
+    for (int i = 0; i < n; i++) {
         printf("\nEnter Student details:\nID: ");
-        scanf("%d",&arr[i].id);
+        scanf("%d", &arr[i].id);
+        
         printf("Name: ");
-        scanf("%s",arr[i].name);
+        scanf("%s", arr[i].name);
+        
         printf("Fees: ");
-        scanf("%f",&arr[i].fees);
+        scanf("%f", &arr[i].fees);
+        
         printf("Branch: ");
-        scanf("%s",arr[i].branch);
+        scanf("%s", arr[i].branch);
     }
+
     printf("\n\nStudent Details:\n");
-    for(int i=0;i<n;i++)
-    printf("ID:%d \t Name:%s \t Fees:%f \t Branch:%s\n",arr[i].id,arr[i].name,arr[i].fees,arr[i].branch);
+    for (int i = 0; i < n; i++)
+        printf("ID:%d \t Name:%s \t Fees:%f \t Branch:%s\n", arr[i].id, arr[i].name, arr[i].fees, arr[i].branch);
+
     return 0;
 }
