91a248c3ea070d05c4b42c22b00eeaa09f243071
M.Sree Abiram <143892160+Abiram116@users.noreply.github.com>
2023-12-01 20:05:10 +0530

    struct

--- in_sem-2/Structures/book.c
+++ in_sem-2/Structures/book.c
@@ -1,1 +1,42 @@
 //define a structure Book having title,author,price and also quantity order. Create a array of n books information. Display the total bill amount.
+#include <stdio.h>
+
+// Structure definition for a book
+struct Book {
+    char title[100];
+    char author[100];
+    float price;
+    int quantity;
+};
+
+int main() {
+    int n; // Number of books
+    float totalBill = 0.0;
+
+    printf("Enter the number of books: ");
+    scanf("%d", &n);
+
+    // Create an array of n books
+    struct Book books[n];
+
+    // Input book information
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for Book %d:\n", i + 1);
+        printf("Title: ");
+        scanf(" %[^\n]s", books[i].title);
+        printf("Author: ");
+        scanf(" %[^\n]s", books[i].author);
+        printf("Price: ");
+        scanf("%f", &books[i].price);
+        printf("Quantity: ");
+        scanf("%d", &books[i].quantity);
+
+        // Calculate the total bill amount for each book
+        totalBill += (books[i].price * books[i].quantity);
+    }
+
+    // Displaying the total bill amount
+    printf("\nTotal Bill Amount: ₹%.2f\n", totalBill);
+
+    return 0;
+}
--- in_sem-2/Structures/items.c
+++ in_sem-2/Structures/items.c
@@ -1,1 +1,47 @@
 //define a structure Item having ID,name,unit of measurement and price. Find number of item whose unit of measurement is literrs
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ITEMS 50 // Maximum number of items
+
+// Structure definition for an item
+struct Item {
+    int ID;
+    char name[100];
+    char unit[4];
+    float price;
+};
+
+int main() {
+    int n; // Number of items
+    int litersCount = 0;
+
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+
+    // Create an array of items
+    struct Item items[MAX_ITEMS];
+
+    // Input item information
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for Item %d:\n", i + 1);
+        printf("ID: ");
+        scanf("%d", &items[i].ID);
+        printf("Name: ");
+        scanf(" %[^\n]s", items[i].name);
+        printf("Unit of measurement: ");
+        scanf(" %[^\n]s", items[i].unit);
+        printf("Price: ");
+        scanf("%f", &items[i].price);
+
+        // Check if the unit of measurement is "liters"
+        if (strcmp(items[i].unit, "l") == 0) {
+            litersCount++;
+        }
+    }
+
+    // Displaying the number of items measured in liters
+    printf("\nNumber of items measured in liters: %d\n", litersCount);
+
+    return 0;
+}
